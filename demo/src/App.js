28e6e574b02095e33bb024301644fe13e7653f2b
ujjawaljha <ujjawaljha08@gmail.com>
2021-08-09 18:59:22 +0530

    up

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./App.css"
 
 class App extends React.Component {
   state = {
@@ -10,20 +11,21 @@ class App extends React.Component {
     return (
       <div>
         <input
+        className="input-box"
           type="text"
           onChange={(e) => {
             this.setState({ currInput: e.currentTarget.value });
           }}
 
           onKeyDown={(e) => {
-            if (e.key === "Enter") {
+            if (e.key == "Enter") {
               this.setState({
                 tasks: [...this.state.tasks, this.state.currInput],
                 currInput: "",
               });
             }
           }}
-          
+
           value={this.state.currInput}
         />
 
